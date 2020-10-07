0667ca6501f04915968551c2d4c3b0247fc340e7
mrpragmatic <subhendra.rpandey@gmail.com>
2020-10-07 18:14:55 +0530

    changed colors

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class BMICalculator extends StatelessWidget {
       //prefix 0xFF with HEX color
       //In this case color was #1D2136
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF1D2136),
-        scaffoldBackgroundColor: Color(0xFF1D2136),
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: InputPage(),
     );

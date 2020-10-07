de1475840bb0724bfcf04a6335b77939c77e4b18
mrpragmatic <subhendra.rpandey@gmail.com>
2020-10-07 18:36:03 +0530

    used const and added bottom container

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -9,51 +13,70 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(child: ReusableCard()),
-                Expanded(
-                  child: ReusableCard(),
-                ),
-              ],
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Expanded(
-              child: ReusableCard(),
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(),
+            Expanded(
+              child: Expanded(
+                child: ReusableCard(
+                  colour: activeCardColor,
                 ),
-                Expanded(
-                  child: ReusableCard(),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: activeCardColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10.0),
+              color: bottomContainerColor,
+              height: bottomContainerHeight,
+              width: double.infinity,
+            )
+          ],
+        ));
   }
 }
 
 class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+  final Color colour;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,
         borderRadius: BorderRadius.circular(16.0),
       ),
     );

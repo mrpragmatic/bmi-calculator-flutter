6a69e8bdf67a622f54c5e8b21a3a8b82db93fc52
mrpragmatic <subhendra.rpandey@gmail.com>
2020-10-08 13:16:44 +0530

    improved main.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,9 +1,8 @@
 import 'package:bmi_calculator/gender_type.dart';
+import 'package:bmi_calculator/icon_content.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import 'icon_content.dart';
-import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -17,106 +16,84 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   GenderType selectedGender;
-  // Color maleCardColor = inActiveCardColor;
-  // Color femaleCardColor = inActiveCardColor;
-
-  // //male = 1, female = 2
-  // void updateColor(GenderType genderType) {
-  //   if (genderType == GenderType.male) {
-  //     if (maleCardColor == inActiveCardColor) {
-  //       maleCardColor = activeCardColor;
-  //       femaleCardColor = inActiveCardColor;
-  //     } else {
-  //       maleCardColor = inActiveCardColor;
-  //     }
-  //   }
-  //   //female
-  //   if (genderType == GenderType.female) {
-  //     if (femaleCardColor == inActiveCardColor) {
-  //       femaleCardColor = activeCardColor;
-  //       maleCardColor = inActiveCardColor;
-  //     } else {
-  //       femaleCardColor = inActiveCardColor;
-  //     }
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BMI CALCULATOR'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ReusableCard(
-                      onPress: () {
-                        setState(() {
-                          selectedGender = GenderType.male;
-                        });
-                      },
-                      colour: selectedGender == GenderType.male
-                          ? activeCardColor
-                          : inActiveCardColor,
-                      cardChild: IconContent(
-                        icon: FontAwesomeIcons.mars,
-                        label: 'MALE',
-                      ),
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = GenderType.male;
+                      });
+                    },
+                    colour: selectedGender == GenderType.male
+                        ? activeCardColor
+                        : inActiveCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
                     ),
                   ),
-                  Expanded(
-                    child: ReusableCard(
-                      onPress: () {
-                        setState(() {
-                          selectedGender = GenderType.female;
-                        });
-                      },
-                      colour: selectedGender == GenderType.female
-                          ? activeCardColor
-                          : inActiveCardColor,
-                      cardChild: IconContent(
-                        icon: FontAwesomeIcons.venus,
-                        label: 'FEMALE',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Expanded(
-                child: ReusableCard(
-                  colour: activeCardColor,
                 ),
-              ),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ReusableCard(
-                      colour: activeCardColor,
+                Expanded(
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = GenderType.female;
+                      });
+                    },
+                    colour: selectedGender == GenderType.female
+                        ? activeCardColor
+                        : inActiveCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
                     ),
                   ),
-                  Expanded(
-                    child: ReusableCard(
-                      colour: activeCardColor,
-                    ),
-                  ),
-                ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Expanded(
+              child: ReusableCard(
+                colour: activeCardColor,
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(top: 10.0),
-              color: bottomContainerColor,
-              height: bottomContainerHeight,
-              width: double.infinity,
-            )
-          ],
-        ));
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            color: bottomContainerColor,
+            height: bottomContainerHeight,
+            width: double.infinity,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:bmi_calculator/input_page.dart';
 import 'package:flutter/material.dart';
-
-import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -8,8 +7,6 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //prefix 0xFF with HEX color
-      //In this case color was #1D2136
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
